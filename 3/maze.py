6dccecdeef7720cf2c6dc0150d9ba777d944d188
Lynn Bradshaw <readyready15728@gmail.com>
2020-10-28 18:30:08 -0400

    Adding Maze class to maze.py

--- 3/maze.py
+++ 3/maze.py
@@ -11,3 +11,28 @@ class Cell:
     PATH = '*'
 
 MazeLocation = namedtuple('MazeLocation', ['row', 'column'])
+
+class Maze:
+    def __init__(self, rows = 10, columns = 10, sparseness = 0.2, start = MazeLocation(0, 0), goal = MazeLocation(9, 9)):
+        self._rows = rows
+        self._columns = columns
+        self.start = start
+        self.goal = goal
+        self._grid = [[Cell.EMPTY for c in range(columns)] for r in range(rows)]
+        self._randomly_fill(rows, columns, sparseness)
+        self._grid[start.row][start.column] = Cell.START
+        self._grid[goal.row][goal.column] = Cell.GOAL
+
+    def _randomly_fill(self, rows, columns, sparseness):
+        for row in range(rows):
+            for column in range(columns):
+                if random.uniform(0, 1) < sparseness:
+                    self._grid[row][column] = Cell.BLOCKED
+
+    def __str__(self):
+        output = []
+
+        for row in self._grid:
+            output.append(''.join([c for c in row]))
+
+        return '\n'.join(output)
